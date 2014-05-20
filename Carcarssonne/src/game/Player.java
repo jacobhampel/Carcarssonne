c4833f72bb6a6a87363b3968f03681e9a679173c
masterhampel <jacob-hampel@gmx.de>
2014-05-20 13:51:42 +0200

    Neuer Commit

--- Carcarssonne/src/game/Player.java
+++ Carcarssonne/src/game/Player.java
@@ -4,63 +4,49 @@ import java.awt.Color;
 import java.util.LinkedList;
 
 public class Player {
-	
+
 	private String name;
 	private int score;
 	private Color color;
-	private static int followerStart= 5;
+	private static int followerStart = 5;
 	private LinkedList<Follower> follower = new LinkedList<Follower>();
-	
-	public Player(String name, Color color)
-	{
+
+	public Player(String name, Color color) {
 		this.name = name;
 		this.color = color;
-		/*for (int i = 0; i < 5; i++) {
-			follower.add(new Follower(this,0,0));
-		}*/
+		/*
+		 * for (int i = 0; i < 5; i++) { follower.add(new Follower(this,0,0)); }
+		 */
 	}
-	
-	public String getName()
-	{
+
+	public String getName() {
 		return name;
 	}
-	public int getScore()
-	{
+
+	public int getScore() {
 		return score;
 	}
-	
-	public Color getColor()
-	{
+
+	public Color getColor() {
 		return color;
 	}
-	
-	public void setScore(int score)
-	{
+
+	public void setScore(int score) {
 		this.score = score;
 	}
-	
-	public Follower getFollower()
-	{
+
+	public Follower getFollower() {
 		Follower f = follower.removeFirst();
 		return f;
 	}
-	
-	public void returnFollower(Follower f)
-	{
+
+	public void returnFollower(Follower f) {
 		follower.add(f);
 	}
-	public int getRemainingFollower()
-	{
-		
-		return followerStart-(follower.size());
+
+	public int getRemainingFollower() {
+
+		return followerStart - (follower.size());
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
--- Carcarssonne/src/main/PlayerMain.java
+++ Carcarssonne/src/main/PlayerMain.java
@@ -7,7 +7,7 @@ public class PlayerMain {
 	
 	public static void main(String[] args)
 	{
-		Game game = new Game(10,10);
+		Game game = new Game(100,100);
 		new GameFrame(game);
 		
 				
